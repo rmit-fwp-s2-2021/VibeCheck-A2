761dc7639ed1de43d08ed04cf3d8a1cef1ada602
abir1997 <s3677701@student.rmit.edu.au>
2021-09-16 02:49:18 +1000

    import routes

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,10 @@ app.get("/", (req, res) => {
   });
 
 
+// Add routes
+require("./src/routes/user.routes.js")(express, app);
+require("./src/routes/post.routes.js")(express, app);
+
 // Set port, listen for requests.
 const PORT = 4000;
 app.listen(PORT, () => {
