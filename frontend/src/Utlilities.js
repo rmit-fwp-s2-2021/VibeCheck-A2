52babd745b5e157bd85b081d3212c4288ec99426
abir1997 <s3677701@student.rmit.edu.au>
2021-09-16 17:39:45 +1000

    Add components

---
+++ frontend/src/Utlilities.js
@@ -0,0 +1,12 @@
+// --- Helper functions to interact with local storage --------------------------------------------
+function setUser(user) {
+  localStorage.setItem(USER_KEY, JSON.stringify(user));
+}
+
+function getUser() {
+  return JSON.parse(localStorage.getItem(USER_KEY));
+}
+
+function removeUser() {
+  localStorage.removeItem(USER_KEY);
+}
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mt-auto py-3 bg-dark text-white">
+      <div className="container">Register, Login and Logout Example &copy; 2021</div>
+    </footer>
+  );
+}
